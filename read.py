f2479ae8daecaf7c2a7e7e14681dda23e151b437
Mathias Falci de Castro <mathias.falci@gmail.com>
2017-05-04 15:04:09 -0300

    libs
    mechanize, beautifulsoup

--- read.py
+++ read.py
@@ -1,7 +1,38 @@
+#import requests
+#from lxml import html
+from bs4 import BeautifulSoup
+import requests
+import mechanize
+import urllib2 
+import cookielib
 
-import json
+#url = "http://megatibia.com/?subtopic=accountmanagement"
 
-with open('data.json') as json_data:
-    d = json.load(json_data)
-    print(d[0].keys())
+#r  = requests.get(url)
+#data = r.text
+#soup = BeautifulSoup(data)
 
+#for link in soup.find_all('a'):
+#print(soup.prettify())
+
+#--------------------
+
+
+
+
+cj = cookielib.CookieJar()
+br = mechanize.Browser()
+br.set_cookiejar(cj)
+br.open(url)
+
+#for f in br.forms():
+#    print f
+#print br.response().read()
+
+print br.select_form(name="account_login=")
+
+#br.select_form(nr=0)
+#br.form['account_login'] = 'username'
+#br.form['password_login'] = 'password.'
+#br.submit()
+#print br.response().read()
